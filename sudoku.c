da5984c7243a2c75795ea207391d6248fc3a1830
gonmf <gonmf@sapo.pt>
2024-07-29 20:12:07 +0100

    perf improvement

--- sudoku.c
+++ sudoku.c
@@ -7,29 +7,34 @@ typedef char bool;
 #define TRUE 1
 #define FALSE 0
 
-static void print_board(const u8 v[81]) {
-    u8 i;
-    for (i = 0; i < 81; ++i) {
-        if (v[i] == 0) {
-            printf(" -");
-        } else {
-            printf(" %u", v[i]);
-        }
+static void print_board(const u8 v[9][9]) {
+    u8 x;
+    u8 y;
 
-        if (((i + 1) % 9) == 0) {
+    for (y = 0; y < 9; ++y) {
+        if (y > 0) {
             printf("\n");
+        }
+        for (x = 0; x < 9; ++x) {
+            if (v[y][x] == 0) {
+                printf(" -");
+            } else {
+                printf(" %u", v[y][x]);
+            }
         }
     }
     printf("\n");
 }
 
-static void string_to_board(u8 v[81], const char * buffer) {
+static void string_to_board(u8 v[9][9], const char * buffer) {
     u8 i;
     for (i = 0; i < 81; ++i) {
+        u8 x = i % 9;
+        u8 y = i / 9;
         if (buffer[i] == '.' || buffer[i] == ' ' || buffer[i] == '0' || buffer[i] == '_') {
-            v[i] = 0;
+            v[y][x] = 0;
         } else {
-            v[i] = buffer[i] - '0';
+            v[y][x] = buffer[i] - '0';
         }
     }
 }
@@ -38,71 +43,64 @@ static u8 rows[9][9];
 static u8 columns[9][9];
 static u8 squares[3][3][9];
 
-static void add_value(u8 v[81], u8 pos, u8 play) {
-    v[pos] = play + 1;
-
-    u8 x = pos % 9;
-    u8 y = pos / 9;
-
-    rows[y][play]++;
-    columns[x][play]++;
-    squares[y / 3][x / 3][play]++;
+static void add_value(u8 v[9][9], u8 x, u8 y, u8 val) {
+    v[y][x] = val + 1;
+    rows[y][val]++;
+    columns[x][val]++;
+    squares[y / 3][x / 3][val]++;
 }
 
-static void rem_value(u8 v[81], u8 pos, u8 play) {
-    v[pos] = 0;
-
-    u8 x = pos % 9;
-    u8 y = pos / 9;
-
-    rows[y][play]--;
-    columns[x][play]--;
-    squares[y / 3][x / 3][play]--;
+static void rem_value(u8 v[9][9], u8 x, u8 y, u8 val) {
+    v[y][x] = 0;
+    rows[y][val]--;
+    columns[x][val]--;
+    squares[y / 3][x / 3][val]--;
 }
 
-static bool solve1(u8 v[81]) {
-    u8 i;
+static bool solve1(u8 v[9][9]) {
     u8 x;
     u8 y;
     u8 best_valid_plays = 10;
-    u8 best_pos;
+    u8 best_pos_x;
+    u8 best_pos_y;
     u8 last_play;
     u8 valid_plays;
     u8 play;
     bool finished = TRUE;
 
-    for (i = 0; i < 81; ++i) {
-        if (v[i] != 0) {
-            continue;
-        }
-
-        finished = FALSE;
-        x = i % 9;
-        y = i / 9;
-
-        last_play = 0;
-        valid_plays = 0;
-        for (play = 0; play < 9; ++play) {
-            if (rows[y][play] == 0 && columns[x][play] == 0 && squares[y / 3][x / 3][play] == 0) {
-                valid_plays++;
-                last_play = play;
+    for (y = 0; y < 9; ++y) {
+        for (x = 0; x < 9; ++x) {
+            if (v[y][x] != 0) {
+                continue;
             }
-        }
 
-        if (valid_plays == 0) {
-            return FALSE;
-        }
-        if (valid_plays == 1) {
-            add_value(v, i, last_play);
-            if (solve1(v)) {
-                return TRUE;
+            finished = FALSE;
+
+            last_play = 0;
+            valid_plays = 0;
+            for (play = 0; play < 9; ++play) {
+                if (rows[y][play] == 0 && columns[x][play] == 0 && squares[y / 3][x / 3][play] == 0) {
+                    valid_plays++;
+                    last_play = play;
+                }
             }
-            rem_value(v, i, last_play);
-            return FALSE;
-        }
-        if (best_valid_plays > valid_plays) {
-            best_valid_plays = valid_plays;
-            best_pos = i;
+
+            if (valid_plays == 0) {
+                return FALSE;
+            }
+            if (valid_plays == 1) {
+                add_value(v, x, y, last_play);
+                if (solve1(v)) {
+                    return TRUE;
+                }
+                rem_value(v, x, y, last_play);
+                return FALSE;
+            }
+            if (best_valid_plays > valid_plays) {
+                best_valid_plays = valid_plays;
+                best_pos_x = x;
+                best_pos_y = y;
+            }
         }
     }
 
@@ -110,42 +108,44 @@ static bool solve1(u8 v[81]) {
         return TRUE;
     }
 
-    x = best_pos % 9;
-    y = best_pos / 9;
+    x = best_pos_x;
+    y = best_pos_y;
     for (play = 0; play < 9; ++play) {
         if (rows[y][play] == 0 && columns[x][play] == 0 && squares[y / 3][x / 3][play] == 0) {
-            add_value(v, best_pos, play);
+            add_value(v, x, y, play);
             if (solve1(v)) {
                 return TRUE;
             }
-            rem_value(v, best_pos, play);
+            rem_value(v, x, y, play);
         }
     }
     return FALSE;
 }
 
-static bool solve(u8 v[81]) {
-    u8 i;
-    u8 j;
+static bool solve(u8 v[9][9]) {
+    u8 x;
+    u8 y;
     u8 k;
 
-    for (i = 0; i < 9; ++i) {
-        for (j = 0; j < 9; ++j) {
-            rows[i][j] = 0;
-            columns[i][j] = 0;
+    for (y = 0; y < 9; ++y) {
+        for (x = 0; x < 9; ++x) {
+            rows[y][x] = 0;
+            columns[y][x] = 0;
         }
     }
-    for (i = 0; i < 3; ++i) {
-        for (j = 0; j < 3; ++j) {
+    for (y = 0; y < 9; ++y) {
+        for (x = 0; x < 9; ++x) {
             for (k = 0; k < 9; ++k) {
-                squares[i][j][k] = 0;
+                squares[y][x][k] = 0;
             }
         }
     }
 
-    for (i = 0; i < 81; ++i) {
-        if (v[i] != 0) {
-            add_value(v, i, v[i] - 1);
+    for (y = 0; y < 9; ++y) {
+        for (x = 0; x < 9; ++x) {
+            if (v[y][x] != 0) {
+                add_value(v, x, y, v[y][x] - 1);
+            }
         }
     }
 
@@ -192,7 +192,7 @@ int main(int argc, char * argv[]) {
           printf("Starting puzzle #%d:\n%s\n\n", number, buffer);
         }
 
-        u8 v[81];
+        u8 v[9][9];
         string_to_board(v, buffer);
 
         bool board_solved = solve(v);
