9de68258b89b7e3355f438da63c46620295d5d52
gonmf <gonmf@sapo.pt>
2024-11-04 19:48:38 +0000

    small improvements

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ At the start it populates the unavailable values for every row/column square, th
 availability, avoiding recalculating them for every square. It prefers searching based on lowest number of possible values for
 each square, mixing in between the detection of impssible states and finished searches, for performance.
 
-Tested with the sample puzzles.txt file (29,072 puzzles) on a 2020 MBP it solves all in 12.37s (0.43ms/puzzle average).
+Tested with the sample puzzles.txt file (29,072 puzzles) on a 2020 MBP it solves all in 12.03s (0.41ms/puzzle average).
--- sudoku.c
+++ sudoku.c
@@ -26,8 +26,9 @@ static u8 calced_valid_plays_count[512];
 static u8 calced_valid_plays[512][8];
 static u8 squares_left_count;
 static Pos squares_left[64];
+static u8 v[9][9];
 
-static void print_board(const u8 v[9][9]) {
+static void print_board() {
     u8 x;
     u8 y;
 
@@ -43,24 +44,10 @@ static void print_board(const u8 v[9][9]) {
             }
         }
     }
-    printf("\n");
+    printf("\n\n");
 }
 
-static void string_to_board(u8 v[9][9], const char * buffer) {
-    u8 i;
-    for (i = 0; i < 81; ++i) {
-        u8 x = i % 9;
-        u8 y = i / 9;
-        if (buffer[i] == '.' || buffer[i] == ' ' || buffer[i] == '0' || buffer[i] == '_') {
-            v[y][x] = 0;
-        } else {
-            v[y][x] = buffer[i] - '0';
-        }
-    }
-}
-
-static void add_value(u8 v[9][9], u8 x, u8 y, u8 val) {
-    v[y][x] = val + 1;
+static void add_value(u8 x, u8 y, u8 val) {
     u16 mask = (1 << val);
     bitmaps.rows[y] |= mask;
     bitmaps.columns[x] |= mask;
@@ -94,11 +81,11 @@ static void init_global() {
     }
 }
 
-static bool search(u8 v[9][9]) {
+static bool search() {
     u8 x;
     u8 y;
     u16 best_masked;
-    u8 best_valid_plays = 10;
+    u8 best_valid_plays = 9;
     u8 best_pos_x;
     u8 best_pos_y;
     u8 square_i;
@@ -117,18 +104,18 @@ static bool search(u8 v[9][9]) {
             return FALSE;
         }
         if (valid_plays == 1) {
-            u8 last_play = calced_valid_plays[masked][0];
+            u8 play = calced_valid_plays[masked][0];
             u16 tmp1 = bitmaps.rows[y];
             u16 tmp2 = bitmaps.columns[x];
             u16 tmp3 = bitmaps.squares[y / 3][x / 3];
             squares_left_count--;
             squares_left[i].x = squares_left[squares_left_count].x;
             squares_left[i].y = squares_left[squares_left_count].y;
-            add_value(v, x, y, last_play);
-            if (search(v)) {
+            add_value(x, y, play);
+            if (search()) {
+                v[y][x] = play + 1;
                 return TRUE;
             }
-            v[y][x] = 0;
             bitmaps.rows[y] = tmp1;
             bitmaps.columns[x] = tmp2;
             bitmaps.squares[y / 3][x / 3] = tmp3;
@@ -155,69 +142,50 @@ static bool search(u8 v[9][9]) {
     u16 tmp2 = bitmaps.columns[x];
     u16 tmp3 = bitmaps.squares[y / 3][x / 3];
 
-    if (best_valid_plays == 9) {
-        for (u8 play = 0; play < 9; ++play) {
-            add_value(v, x, y, play);
-            squares_left_count--;
-            squares_left[square_i].x = squares_left[squares_left_count].x;
-            squares_left[square_i].y = squares_left[squares_left_count].y;
-            if (search(v)) {
-                return TRUE;
-            }
-            bitmaps.rows[y] = tmp1;
-            bitmaps.columns[x] = tmp2;
-            bitmaps.squares[y / 3][x / 3] = tmp3;
-            squares_left[squares_left_count].x = squares_left[square_i].x;
-            squares_left[squares_left_count].y = squares_left[square_i].y;
-            squares_left[square_i].x = x;
-            squares_left[square_i].y = y;
-            squares_left_count++;
-        }
-        v[y][x] = 0;
-        return FALSE;
-    }
-
+    squares_left_count--;
     for (char i = best_valid_plays - 1; i >= 0; --i) {
         u8 play = calced_valid_plays[best_masked][(u8)i];
-        squares_left_count--;
         squares_left[square_i].x = squares_left[squares_left_count].x;
         squares_left[square_i].y = squares_left[squares_left_count].y;
-        add_value(v, x, y, play);
-        if (search(v)) {
+        add_value(x, y, play);
+        if (search()) {
+            v[y][x] = play + 1;
             return TRUE;
         }
         bitmaps.rows[y] = tmp1;
         bitmaps.columns[x] = tmp2;
         bitmaps.squares[y / 3][x / 3] = tmp3;
-        squares_left[squares_left_count].x = squares_left[square_i].x;
-        squares_left[squares_left_count].y = squares_left[square_i].y;
-        squares_left[square_i].x = x;
-        squares_left[square_i].y = y;
-        squares_left_count++;
     }
-    v[y][x] = 0;
+    squares_left[square_i].x = x;
+    squares_left[square_i].y = y;
+    squares_left_count++;
     return FALSE;
 }
 
-static bool init_board(u8 v[9][9]) {
+static bool init_board(const char * buffer) {
     memcpy(&bitmaps, &start_bitmaps, sizeof(Bitmaps));
 
     squares_left_count = 0;
 
+    u8 i = 0;
     for (u8 y = 0; y < 9; ++y) {
         for (u8 x = 0; x < 9; ++x) {
-            u8 vs = v[y][x];
-            if (vs == 0) {
+            if (buffer[i] == '.') {
+                v[y][x] = 0;
                 squares_left[squares_left_count].x = x;
                 squares_left[squares_left_count].y = y;
                 squares_left_count++;
             } else {
-                add_value(v, x, y, vs - 1);
+                u8 vs = buffer[i] - '0';
+                add_value(x, y, vs - 1);
+                v[y][x] = vs;
             }
+
+            i++;
         }
     }
 
-    return search(v);
+    return search();
 }
 
 int main(int argc, char * argv[]) {
@@ -227,7 +195,6 @@ int main(int argc, char * argv[]) {
     }
     unsigned int number = 0;
     unsigned int solved = 0;
-    unsigned int impossible = 0;
     bool verbose = FALSE;
 
     const char * filename = argv[1];
@@ -261,23 +228,19 @@ int main(int argc, char * argv[]) {
           printf("Starting puzzle #%d:\n%s\n\n", number, buffer);
         }
 
-        u8 v[9][9];
-        string_to_board(v, buffer);
-
-        bool board_solved = init_board(v);
+        bool board_solved = init_board(buffer);
         if (board_solved) {
             solved++;
             if (verbose) {
                 printf("Solution:\n");
-                print_board(v);
+                print_board();
             }
         } else {
-            impossible++;
             if (verbose) {
                 fprintf(stderr, "Impossible puzzle\n");
             }
         }
     }
-    printf("Solved=%u\nImpossible=%u\nTotal=%u\n", solved, impossible, number);
+    printf("Solved %u / %u\n", solved, number);
     return EXIT_SUCCESS;
 }
